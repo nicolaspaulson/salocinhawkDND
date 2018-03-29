753a8d300dc2da7528d05ca38fd4d3fd0c715e3f
Nic <nicolas.paulson@tecuity.com>
2018-03-29 12:01:50 -0600

    Added form skeleton

--- salocinhawkDND/Form1.Designer.cs
+++ salocinhawkDND/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace salocinhawkDND
 {
-    partial class Form1
+    partial class DND
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,321 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.rollButton = new System.Windows.Forms.Button();
+            this.nameText1 = new System.Windows.Forms.TextBox();
+            this.nameText2 = new System.Windows.Forms.TextBox();
+            this.nameText3 = new System.Windows.Forms.TextBox();
+            this.nameText4 = new System.Windows.Forms.TextBox();
+            this.nameText5 = new System.Windows.Forms.TextBox();
+            this.skillBox1 = new System.Windows.Forms.ComboBox();
+            this.bonusText1 = new System.Windows.Forms.TextBox();
+            this.bonusText2 = new System.Windows.Forms.TextBox();
+            this.bonusText3 = new System.Windows.Forms.TextBox();
+            this.bonusText4 = new System.Windows.Forms.TextBox();
+            this.bonusText5 = new System.Windows.Forms.TextBox();
+            this.passFailText1 = new System.Windows.Forms.TextBox();
+            this.passFailText2 = new System.Windows.Forms.TextBox();
+            this.passFailText3 = new System.Windows.Forms.TextBox();
+            this.passFailText4 = new System.Windows.Forms.TextBox();
+            this.passFailText5 = new System.Windows.Forms.TextBox();
+            this.globalBox1 = new System.Windows.Forms.CheckBox();
+            this.globalBox2 = new System.Windows.Forms.CheckBox();
+            this.globalBox3 = new System.Windows.Forms.CheckBox();
+            this.globalBox4 = new System.Windows.Forms.CheckBox();
+            this.globalBox5 = new System.Windows.Forms.CheckBox();
+            this.globalBox6 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // rollButton
+            // 
+            this.rollButton.Location = new System.Drawing.Point(145, 357);
+            this.rollButton.Margin = new System.Windows.Forms.Padding(6);
+            this.rollButton.Name = "rollButton";
+            this.rollButton.Size = new System.Drawing.Size(458, 47);
+            this.rollButton.TabIndex = 0;
+            this.rollButton.Text = "ROLL";
+            this.rollButton.UseVisualStyleBackColor = true;
+            // 
+            // nameText1
+            // 
+            this.nameText1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameText1.Location = new System.Drawing.Point(15, 15);
+            this.nameText1.Margin = new System.Windows.Forms.Padding(6);
+            this.nameText1.Name = "nameText1";
+            this.nameText1.ReadOnly = true;
+            this.nameText1.Size = new System.Drawing.Size(414, 29);
+            this.nameText1.TabIndex = 1;
+            // 
+            // nameText2
+            // 
+            this.nameText2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameText2.Location = new System.Drawing.Point(15, 65);
+            this.nameText2.Margin = new System.Windows.Forms.Padding(6);
+            this.nameText2.Name = "nameText2";
+            this.nameText2.ReadOnly = true;
+            this.nameText2.Size = new System.Drawing.Size(414, 29);
+            this.nameText2.TabIndex = 2;
+            // 
+            // nameText3
+            // 
+            this.nameText3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameText3.Location = new System.Drawing.Point(15, 117);
+            this.nameText3.Margin = new System.Windows.Forms.Padding(6);
+            this.nameText3.Name = "nameText3";
+            this.nameText3.ReadOnly = true;
+            this.nameText3.Size = new System.Drawing.Size(414, 29);
+            this.nameText3.TabIndex = 3;
+            // 
+            // nameText4
+            // 
+            this.nameText4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameText4.Location = new System.Drawing.Point(15, 169);
+            this.nameText4.Margin = new System.Windows.Forms.Padding(6);
+            this.nameText4.Name = "nameText4";
+            this.nameText4.ReadOnly = true;
+            this.nameText4.Size = new System.Drawing.Size(414, 29);
+            this.nameText4.TabIndex = 4;
+            // 
+            // nameText5
+            // 
+            this.nameText5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameText5.Location = new System.Drawing.Point(15, 220);
+            this.nameText5.Margin = new System.Windows.Forms.Padding(6);
+            this.nameText5.Name = "nameText5";
+            this.nameText5.ReadOnly = true;
+            this.nameText5.Size = new System.Drawing.Size(414, 29);
+            this.nameText5.TabIndex = 5;
+            // 
+            // skillBox1
+            // 
+            this.skillBox1.FormattingEnabled = true;
+            this.skillBox1.Location = new System.Drawing.Point(15, 273);
+            this.skillBox1.Name = "skillBox1";
+            this.skillBox1.Size = new System.Drawing.Size(250, 33);
+            this.skillBox1.TabIndex = 7;
+            // 
+            // bonusText1
+            // 
+            this.bonusText1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bonusText1.Location = new System.Drawing.Point(474, 15);
+            this.bonusText1.Margin = new System.Windows.Forms.Padding(6);
+            this.bonusText1.Name = "bonusText1";
+            this.bonusText1.ReadOnly = true;
+            this.bonusText1.Size = new System.Drawing.Size(65, 29);
+            this.bonusText1.TabIndex = 8;
+            // 
+            // bonusText2
+            // 
+            this.bonusText2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bonusText2.Location = new System.Drawing.Point(474, 65);
+            this.bonusText2.Margin = new System.Windows.Forms.Padding(6);
+            this.bonusText2.Name = "bonusText2";
+            this.bonusText2.ReadOnly = true;
+            this.bonusText2.Size = new System.Drawing.Size(65, 29);
+            this.bonusText2.TabIndex = 9;
+            // 
+            // bonusText3
+            // 
+            this.bonusText3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bonusText3.Location = new System.Drawing.Point(474, 117);
+            this.bonusText3.Margin = new System.Windows.Forms.Padding(6);
+            this.bonusText3.Name = "bonusText3";
+            this.bonusText3.ReadOnly = true;
+            this.bonusText3.Size = new System.Drawing.Size(65, 29);
+            this.bonusText3.TabIndex = 10;
+            // 
+            // bonusText4
+            // 
+            this.bonusText4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bonusText4.Location = new System.Drawing.Point(474, 169);
+            this.bonusText4.Margin = new System.Windows.Forms.Padding(6);
+            this.bonusText4.Name = "bonusText4";
+            this.bonusText4.ReadOnly = true;
+            this.bonusText4.Size = new System.Drawing.Size(65, 29);
+            this.bonusText4.TabIndex = 11;
+            // 
+            // bonusText5
+            // 
+            this.bonusText5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bonusText5.Location = new System.Drawing.Point(474, 220);
+            this.bonusText5.Margin = new System.Windows.Forms.Padding(6);
+            this.bonusText5.Name = "bonusText5";
+            this.bonusText5.ReadOnly = true;
+            this.bonusText5.Size = new System.Drawing.Size(65, 29);
+            this.bonusText5.TabIndex = 12;
+            // 
+            // passFailText1
+            // 
+            this.passFailText1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passFailText1.Location = new System.Drawing.Point(596, 15);
+            this.passFailText1.Margin = new System.Windows.Forms.Padding(6);
+            this.passFailText1.Name = "passFailText1";
+            this.passFailText1.ReadOnly = true;
+            this.passFailText1.Size = new System.Drawing.Size(162, 29);
+            this.passFailText1.TabIndex = 13;
+            // 
+            // passFailText2
+            // 
+            this.passFailText2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passFailText2.Location = new System.Drawing.Point(596, 65);
+            this.passFailText2.Margin = new System.Windows.Forms.Padding(6);
+            this.passFailText2.Name = "passFailText2";
+            this.passFailText2.ReadOnly = true;
+            this.passFailText2.Size = new System.Drawing.Size(162, 29);
+            this.passFailText2.TabIndex = 14;
+            // 
+            // passFailText3
+            // 
+            this.passFailText3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passFailText3.Location = new System.Drawing.Point(596, 117);
+            this.passFailText3.Margin = new System.Windows.Forms.Padding(6);
+            this.passFailText3.Name = "passFailText3";
+            this.passFailText3.ReadOnly = true;
+            this.passFailText3.Size = new System.Drawing.Size(162, 29);
+            this.passFailText3.TabIndex = 15;
+            // 
+            // passFailText4
+            // 
+            this.passFailText4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passFailText4.Location = new System.Drawing.Point(596, 169);
+            this.passFailText4.Margin = new System.Windows.Forms.Padding(6);
+            this.passFailText4.Name = "passFailText4";
+            this.passFailText4.ReadOnly = true;
+            this.passFailText4.Size = new System.Drawing.Size(162, 29);
+            this.passFailText4.TabIndex = 16;
+            // 
+            // passFailText5
+            // 
+            this.passFailText5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passFailText5.Location = new System.Drawing.Point(596, 220);
+            this.passFailText5.Margin = new System.Windows.Forms.Padding(6);
+            this.passFailText5.Name = "passFailText5";
+            this.passFailText5.ReadOnly = true;
+            this.passFailText5.Size = new System.Drawing.Size(162, 29);
+            this.passFailText5.TabIndex = 17;
+            // 
+            // globalBox1
+            // 
+            this.globalBox1.AutoSize = true;
+            this.globalBox1.Location = new System.Drawing.Point(329, 273);
+            this.globalBox1.Name = "globalBox1";
+            this.globalBox1.Size = new System.Drawing.Size(139, 30);
+            this.globalBox1.TabIndex = 18;
+            this.globalBox1.Text = "globalBox1";
+            this.globalBox1.UseVisualStyleBackColor = true;
+            // 
+            // globalBox2
+            // 
+            this.globalBox2.AutoSize = true;
+            this.globalBox2.Location = new System.Drawing.Point(474, 273);
+            this.globalBox2.Name = "globalBox2";
+            this.globalBox2.Size = new System.Drawing.Size(139, 30);
+            this.globalBox2.TabIndex = 19;
+            this.globalBox2.Text = "globalBox2";
+            this.globalBox2.UseVisualStyleBackColor = true;
+            // 
+            // globalBox3
+            // 
+            this.globalBox3.AutoSize = true;
+            this.globalBox3.Location = new System.Drawing.Point(618, 273);
+            this.globalBox3.Name = "globalBox3";
+            this.globalBox3.Size = new System.Drawing.Size(139, 30);
+            this.globalBox3.TabIndex = 20;
+            this.globalBox3.Text = "globalBox3";
+            this.globalBox3.UseVisualStyleBackColor = true;
+            // 
+            // globalBox4
+            // 
+            this.globalBox4.AutoSize = true;
+            this.globalBox4.Location = new System.Drawing.Point(329, 309);
+            this.globalBox4.Name = "globalBox4";
+            this.globalBox4.Size = new System.Drawing.Size(139, 30);
+            this.globalBox4.TabIndex = 21;
+            this.globalBox4.Text = "globalBox4";
+            this.globalBox4.UseVisualStyleBackColor = true;
+            // 
+            // globalBox5
+            // 
+            this.globalBox5.AutoSize = true;
+            this.globalBox5.Location = new System.Drawing.Point(475, 309);
+            this.globalBox5.Name = "globalBox5";
+            this.globalBox5.Size = new System.Drawing.Size(139, 30);
+            this.globalBox5.TabIndex = 22;
+            this.globalBox5.Text = "globalBox5";
+            this.globalBox5.UseVisualStyleBackColor = true;
+            // 
+            // globalBox6
+            // 
+            this.globalBox6.AutoSize = true;
+            this.globalBox6.Location = new System.Drawing.Point(619, 309);
+            this.globalBox6.Name = "globalBox6";
+            this.globalBox6.Size = new System.Drawing.Size(139, 30);
+            this.globalBox6.TabIndex = 23;
+            this.globalBox6.Text = "globalBox6";
+            this.globalBox6.UseVisualStyleBackColor = true;
+            // 
+            // DND
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(777, 419);
+            this.Controls.Add(this.globalBox6);
+            this.Controls.Add(this.globalBox5);
+            this.Controls.Add(this.globalBox4);
+            this.Controls.Add(this.globalBox3);
+            this.Controls.Add(this.globalBox2);
+            this.Controls.Add(this.globalBox1);
+            this.Controls.Add(this.passFailText5);
+            this.Controls.Add(this.passFailText4);
+            this.Controls.Add(this.passFailText3);
+            this.Controls.Add(this.passFailText2);
+            this.Controls.Add(this.passFailText1);
+            this.Controls.Add(this.bonusText5);
+            this.Controls.Add(this.bonusText4);
+            this.Controls.Add(this.bonusText3);
+            this.Controls.Add(this.bonusText2);
+            this.Controls.Add(this.bonusText1);
+            this.Controls.Add(this.skillBox1);
+            this.Controls.Add(this.nameText5);
+            this.Controls.Add(this.nameText4);
+            this.Controls.Add(this.nameText3);
+            this.Controls.Add(this.nameText2);
+            this.Controls.Add(this.nameText1);
+            this.Controls.Add(this.rollButton);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "DND";
+            this.Text = "DnD Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button rollButton;
+        private System.Windows.Forms.TextBox nameText1;
+        private System.Windows.Forms.TextBox nameText2;
+        private System.Windows.Forms.TextBox nameText3;
+        private System.Windows.Forms.TextBox nameText4;
+        private System.Windows.Forms.TextBox nameText5;
+        private System.Windows.Forms.ComboBox skillBox1;
+        private System.Windows.Forms.TextBox bonusText1;
+        private System.Windows.Forms.TextBox bonusText2;
+        private System.Windows.Forms.TextBox bonusText3;
+        private System.Windows.Forms.TextBox bonusText4;
+        private System.Windows.Forms.TextBox bonusText5;
+        private System.Windows.Forms.TextBox passFailText1;
+        private System.Windows.Forms.TextBox passFailText2;
+        private System.Windows.Forms.TextBox passFailText3;
+        private System.Windows.Forms.TextBox passFailText4;
+        private System.Windows.Forms.TextBox passFailText5;
+        private System.Windows.Forms.CheckBox globalBox1;
+        private System.Windows.Forms.CheckBox globalBox2;
+        private System.Windows.Forms.CheckBox globalBox3;
+        private System.Windows.Forms.CheckBox globalBox4;
+        private System.Windows.Forms.CheckBox globalBox5;
+        private System.Windows.Forms.CheckBox globalBox6;
     }
 }
 
--- salocinhawkDND/Form1.cs
+++ salocinhawkDND/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace salocinhawkDND
 {
-    public partial class Form1 : Form
+    public partial class DND : Form
     {
-        public Form1()
+        public DND()
         {
             InitializeComponent();
         }
